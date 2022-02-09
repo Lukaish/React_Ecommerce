886f245d9bb447b27593813f1dce38a469792ebc
Lukaish <lukassilvabernardi@outlook.com>
2022-02-09 14:22:38 -0300

    ecommerce03022022

---
+++ src/pages/shop/index.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import Menu from "../../components/Menu";
+import Link from "@material-ui/core/Link";
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from '@material-ui/core/Paper';
+
+import { useShop } from "../../context/shop";
+import { FormatCurrency } from "../../utils/formatCurrency";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function Shop() {
+  const { productsShop, setProductsShop } = useShop();
+
+  useEffect(() => {
+    const formatData = productsShop.map((prod) => {
+      return { ...prod, valueFormatted: FormatCurrency(prod.value) };
+    });
+    setProductsShop(formatData);
+  }, []);
+
+  // function removeInfoItem(index: Table.length) {
+  //   const itensCopy = Array.from(infos);
+
+  //   itensCopy.splice(index, 1);
+
+  //   setInfos(itensCopy);
+  // }
+
+  return (
+    <React.Fragment>
+      <Menu />
+      <div>
+        <Typography variant="h5" component="h2">
+          Sua Sacola
+        </Typography>
+        <Table>
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell>Item</StyledTableCell>
+              <StyledTableCell align="right">Valor Unitário</StyledTableCell>
+              <StyledTableCell align="right">Descrição</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {productsShop.map((product) => (
+              <StyledTableRow key={product.id}>
+                <StyledTableCell>{product.title}</StyledTableCell>
+                <StyledTableCell align="right">{product.valueFormatted}</StyledTableCell>
+                <StyledTableCell align="right">{product.description}</StyledTableCell>
+              </StyledTableRow>           
+            ))}            
+          </TableBody>
+          
+        </Table>
+      </div>
+    </React.Fragment>
+  );
+  
+}
+
+export default Shop;
